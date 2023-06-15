0f74918572de39e1d6a6c62fd97c32f1b70338f1
Sheggybtw <pschenichniyvitaliy@gmail.com>
2023-06-15 14:15:33 +0300

    minor changes

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import User from './User';
+import {HashRouter} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 document.title = 'SnAke GaMe';
 
 root.render(
-  <>
+  <HashRouter>
     <App />
     <User />
-  </>
+  </HashRouter>
 );
 
