2341f78906274f68881992e3c995914d018c405c
Sheggybtw <pschenichniyvitaliy@gmail.com>
2023-06-15 15:54:55 +0300

    UseEffect update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import Food from "./components/Food";
 import Snake from "./components/Snake";
-import User from "./User";
 
 const randomFoodPosition = () => {
   const pos = { x: 0, y: 0 };
   let x = Math.floor(Math.random() * 96);
   let y = Math.floor(Math.random() * 96);
   pos.x = x - (x % 4);
-  pos.y = y - (y % 4); 
+  pos.y = y - (y % 4);
   return pos;
 };
 
@@ -18,13 +17,12 @@ const extraRandomFoodPosition = () => {
   let x = Math.floor(Math.random() * 96);
   let y = Math.floor(Math.random() * 96);
   pos.x = x - (x % 4);
-  pos.y = y - (y % 4); 
+  pos.y = y - (y % 4);
   return pos;
 };
 
 function App() {
-
-  let initialSnake = {
+  const initialSnake = {
     snake: [
       { x: 0, y: 0 },
       { x: 4, y: 0 },
@@ -44,10 +42,6 @@ function App() {
   const playgroundRef = useRef();
   const [isPaused, setIsPaused] = useState(false);
   const [score, setScore] = useState(0);
-
-  if (score === 50 || score === 100) {
-    initialSnake.speed -= 50;
-  }
 
   useEffect(() => {
     if (!isStarted) return;
@@ -90,97 +84,105 @@ function App() {
       return;
     }
 
+    const move = () => {
+      const tmpSnake = [...snake];
+      let x = tmpSnake[tmpSnake.length - 1].x,
+        y = tmpSnake[tmpSnake.length - 1].y;
+
+      switch (lastDirection) {
+        case "ArrowUp":
+          y -= 4;
+          break;
+        case "ArrowRight":
+          x += 4;
+          break;
+        case "ArrowDown":
+          y += 4;
+          break;
+        case "ArrowLeft":
+          x -= 4;
+          break;
+        default:
+          break;
+      }
+
+      const newHead = { x, y };
+      const collidedWithSelf = tmpSnake.some(
+        (bodyPart) => bodyPart.x === newHead.x && bodyPart.y === newHead.y
+      );
+
+      if (
+        x !== foodPosition.x ||
+        y !== foodPosition.y ||
+        collidedWithSelf
+      ) {
+        tmpSnake.shift();
+      } else {
+        setFoodPosition(randomFoodPosition());
+        setScore((prevScore) => prevScore + 1);
+        if ((score + 1) % 4 === 0) {
+          setExtraFoodPosition(extraRandomFoodPosition());
+        }
+
+        if ((score + 1) % 6 === 0) {
+          setBonusExtraFoodPosition(extraRandomFoodPosition());
+        }
+      }
+
+      if (
+        extraFoodPosition &&
+        x === extraFoodPosition.x &&
+        y === extraFoodPosition.y
+      ) {
+        tmpSnake.unshift(tmpSnake[0]);
+        setExtraFoodPosition(null);
+        setScore((prevScore) => prevScore + 5);
+      }
+
+      if (
+        bonusExtraFoodPosition &&
+        x === bonusExtraFoodPosition.x &&
+        y === bonusExtraFoodPosition.y
+      ) {
+        tmpSnake.unshift(tmpSnake[0]);
+        setBonusExtraFoodPosition(null);
+        setScore((prevScore) => prevScore + 10);
+      }
+
+      if (collidedWithSelf) {
+        setGameOver(true);
+        return;
+      }
+
+      tmpSnake.push(newHead);
+      setSnake(tmpSnake);
+    };
+
     const interval = setInterval(move, initialSnake.speed);
     return () => clearInterval(interval);
-  }, [isStarted, isPaused, snake]);
+  }, [
+    isStarted,
+    isPaused,
+    snake,
+    initialSnake.speed,
+    lastDirection,
+    foodPosition,
+    extraFoodPosition,
+    bonusExtraFoodPosition,
+    score,
+  ]);
 
   const togglePause = () => {
     setIsPaused(!isPaused);
   };
 
-  const move = () => {
-    const tmpSnake = [...snake];
-    let x = tmpSnake[tmpSnake.length - 1].x,
-      y = tmpSnake[tmpSnake.length - 1].y;
-
-    switch (lastDirection) {
-      case "ArrowUp":
-        y -= 4;
-        break;
-      case "ArrowRight":
-        x += 4;
-        break;
-      case "ArrowDown":
-        y += 4;
-        break;
-      case "ArrowLeft":
-        x -= 4;
-        break;
-      default:
-        break;
-    }
-
-    const newHead = { x, y };
-    const collidedWithSelf = tmpSnake.some(
-      (bodyPart) => bodyPart.x === newHead.x && bodyPart.y === newHead.y
-    );
-
-    if (
-      x !== foodPosition.x ||
-      y !== foodPosition.y ||
-      collidedWithSelf
-    ) {
-      tmpSnake.shift();
-    } else {
-      setFoodPosition(randomFoodPosition());
-      setScore((prevScore) => prevScore + 1);
-      if ((score + 1) % 4 === 0) {
-        setExtraFoodPosition(extraRandomFoodPosition());
-      }
-
-      if ((score + 1) % 6 === 0) {
-        setBonusExtraFoodPosition(extraRandomFoodPosition());
-      }
-    }
-
-    if (
-      extraFoodPosition &&
-      x === extraFoodPosition.x &&
-      y === extraFoodPosition.y
-    ) {
-      tmpSnake.unshift(tmpSnake[0]);
-      setExtraFoodPosition(null);
-      setScore((prevScore) => prevScore + 5);
-    }
-
-    if (
-      bonusExtraFoodPosition &&
-      x === bonusExtraFoodPosition.x &&
-      y === bonusExtraFoodPosition.y
-    ) {
-      tmpSnake.unshift(tmpSnake[0]);
-      setBonusExtraFoodPosition(null);
-      setScore((prevScore) => prevScore + 10);
-    }
-
-    if (collidedWithSelf) {
-      setGameOver(true);
-      return;
-    }
-
-    tmpSnake.push(newHead);
-    setSnake(tmpSnake);
-  };
-
   return (
     <div
       className="App"
-      onKeyDown={(e) => setLastDirection(e.key)}
+      onKeyPress={(e) => setLastDirection(e.key)}
       ref={playgroundRef}
       tabIndex={0}
     >
-      {User}
-
       {isStarted && <div className="count">score: {score}</div>}
 
       {!isStarted && (
@@ -220,14 +222,14 @@ function App() {
 
       <Snake snake={snake} lastDirection={lastDirection} />
 
-      {!gameOver && <Food position={foodPosition} />}
+      {!gameOver && <Food position={foodPosition} key="food" />}
 
       {!gameOver && extraFoodPosition && (
-        <Food position={extraFoodPosition}  isExtraFood={true} />
+        <Food position={extraFoodPosition} isExtraFood={true} key="extra-food" />
       )}
 
       {!gameOver && bonusExtraFoodPosition && (
-        <Food position={bonusExtraFoodPosition}  isExtraFood={true} />
+        <Food position={bonusExtraFoodPosition} isExtraFood={true} key="bonus-food" />
       )}
 
       {!gameOver && !isPaused && (
